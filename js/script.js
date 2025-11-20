c910238a17760da1d68525523357b55c068e70d7
Chris Watnee <chriswatnee@users.noreply.github.com>
2025-11-20 10:32:57 -0500

    Add addPagination function

--- js/script.js
+++ js/script.js
@@ -35,4 +35,39 @@ function showPage(list, page) {
   }
 }
 
+// Function responsible for rendering the pagination buttons to the page
+function addPagination(list) {
+  const numberOfButtons = Math.ceil(list.length / studentsPerPage);
+  const linkList = document.querySelector(".link-list");
+  // Remove any buttons that might have previously been displayed
+  linkList.innerHTML = "";
+  // Loop over the number of buttons
+  for (let i = 1; i <= numberOfButtons; i++) {
+      // Create button html
+    const html = `
+      <li>
+        <button type="button">${i}</button>
+      </li>
+    `;
+    // Insert the html into the .link-list ul
+    linkList.insertAdjacentHTML("beforeend", html);
+  }
+  // Select the first button and give it a class name of active
+  linkList.querySelector("button").className = "active";
+  // Event listener to listen for clicks on the .link-list ul
+  linkList.addEventListener("click", (event) => {
+    // Conditional checks if the clicked element is a button
+    if (event.target.tagName === "BUTTON") {
+      const clickedButton = event.target;
+      const activeButton = linkList.querySelector(".active");
+      // Remove the active class from any other button
+      activeButton.classList.remove("active");
+      // Add the active class to the button that was just clicked
+      clickedButton.classList.add("active");
+      // Call the showPage function and pass it the list and page number
+      showPage(data, clickedButton.textContent);
+    }
+  });
+}
+
 // Call functions
